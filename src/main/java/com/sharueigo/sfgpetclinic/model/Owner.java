76a71ef1f746b17490d72871ec5acf4240d5857d
Sheryaar <Sheryaar.butt@gmail.com>
2020-06-05 18:37:29 -0400

    adding POJOs. Closing #2

---
+++ src/main/java/com/sharueigo/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.sharueigo.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/sharueigo/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.sharueigo.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
