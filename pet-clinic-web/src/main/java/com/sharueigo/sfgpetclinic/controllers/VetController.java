adb17471c61e80d30ef0a39306bf590c20489e6c
Sheryaar <Sheryaar.butt@gmail.com>
2020-06-07 15:10:18 -0400

    List of vets added to vets index. Closes #15

--- pet-clinic-web/src/main/java/com/sharueigo/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/sharueigo/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package com.sharueigo.sfgpetclinic.controllers;
 
+import com.sharueigo.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"","/index.html","/index"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
+
         return "vets/index";
     }
 
